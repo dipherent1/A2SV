643a24d6a7dc31c7fb74aa0538e9d3f833590b1e
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2023-10-08 09:18:12 +0300

    added zigzag-conversion.python

--- zigzag-conversion.py
+++ zigzag-conversion.py
@@ -1,11 +1,21 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows ==1: return s
+        if numRows == 1: return s
+
+        row_list = {row:"" for row in range(numRows)}
+        # print(row_list)
+        up = True
+        row = 0
+
+        for l in s:
+            row_list[row] += l
+            if row < numRows-1 and up or row == 0:
+                row+=1
+                up = True
+            else:
+                up = False
+                row-=1
         res = ""
-        for r in range(numRows):
-            inc = 2*(numRows-1)
-            for i in range(r,len(s),inc):
-                res+=s[i]
-                if (r>0 and r<numRows-1 and i + inc-2*r <len(s)):
-                    res+=s[ i + inc-2*r]
+        for rows in row_list:
+            res+=row_list[rows]
         return res
