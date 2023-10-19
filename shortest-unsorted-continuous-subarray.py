23a0de4e1e7b2a604ce86bed4ea621b2f6de2588
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2023-10-20 00:00:08 +0300

    added shortest-unsorted-continuous-subarray.python

---
+++ shortest-unsorted-continuous-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)  
+        start, end = len(nums), 0  
+        
+       
+        for i in range(len(nums)):
+            if nums[i] != sorted_nums[i]:
+                start = min(start, i)
+                end = max(end, i)
+        
+        return end - start + 1 if end - start >= 0 else 0
