600f8432943c6c377b814ddb3bd29c9c5d3cacca
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2023-11-15 20:11:40 +0300

    added partition-labels.python

--- partition-labels.py
+++ partition-labels.py
@@ -1,18 +1,47 @@
-from typing import List
-
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        l = len(s)
-        res = []
-        i = 0
-        last = {val: i for i, val in enumerate(s)}
-        while i < l:
-            end = last[s[i]]
-            j = i
-            while j < end:
-                if last[s[j]] > end:
-                    end = last[s[j]]
-                j+=1
-            res.append(end-i+1)
-            i = end+1
-        return res
+        '''
+        "aba defde hijhij"
+         
+        "eccbbbbdec"
+
+        # 2a  3
+        # 1b
+        
+        # 2d
+        # 2e
+        # 1f 5
+
+        # 2h
+        # 2i
+        # 2j 6
+        
+        2a
+        1b
+        
+        2d
+        2e
+        1f
+        
+        2h
+        2i
+        2j
+
+        '''
+        count = Counter(s)
+        sub_count = defaultdict(int)
+        ans = []
+        # is_partition = True
+        
+        for char in s:
+            sub_count[char] = 1+sub_count.get(char,0)
+            is_partition = True
+            for key, value in sub_count.items():
+                if count[key]!=value:
+                    is_partition = False
+                    break
+            if is_partition:
+                ans.append(sum(sub_count.values()))
+                sub_count = defaultdict(int)
+                # is_partition = True
+        return ans
