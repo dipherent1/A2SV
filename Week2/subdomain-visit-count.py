3fc6c337fccf0128638bd4ac45dc74bdbb9e1157
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2023-12-08 19:11:43 +0300

    Added solution for Subdomain Visit Count

--- Week2/subdomain-visit-count.py
+++ Week2/subdomain-visit-count.py
@@ -1,19 +1,18 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        mp = {}
-        for cp in cpdomains:
-            num,domains = cp.split(" ")
-            # domains = domains.split(".")
-            mp[domains] = mp.get(domains,0)+int(num)
-            for i in range(len(cp)-1,-1,-1):
-                if cp[i] == ".":
-                    mp[cp[i+1:]] = mp.get(cp[i+1:],0)+int(num)
-        ans = []
-        # print(mp)
-        for key, values in mp.items():
-            ans.append(f"{values} {key}")
-        return ans
+        dic = {}
+        res = []
+        for i in cpdomains:
+            rep, d = i.split()
+            dr =d.split(".")
+            big=0
+            while big < len(dr):
+                if  ".".join(dr[big:]) in dic:
+                    dic[".".join(dr[big:])] +=int(rep)
 
-
-
-        
+                else:
+                    dic[".".join(dr[big:])] = int(rep) 
+                big+=1
+        for key,value in dic.items():
+            res.append(f"{value} {key}")
+        return res
