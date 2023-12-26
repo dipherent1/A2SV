47e007ba2859f6d784fbc450aedbe3f117ad1d0d
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2023-12-26 18:32:54 +0300

    Added solution for Sort the People

--- progress_sheet/week 1/sort-the-people.py
+++ progress_sheet/week 1/sort-the-people.py
@@ -1,7 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         for i in range(len(names)):
-            for j in range(len(names)-1-i):
+            for j in range(len(names)-1):
                 if heights[j]<heights[j+1]:
                     heights[j],heights[j+1] = heights[j+1],heights[j]
                     names[j],names[j+1] = names[j+1],names[j]
