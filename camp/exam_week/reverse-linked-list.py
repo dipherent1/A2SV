07d88691767d56ebe6ed489d86f990b20617d307
dipherent1 <121766117+dipherent1@users.noreply.github.com>
2024-01-24 18:33:22 +0300

    Added solution for Reverse Linked List

--- camp/exam_week/reverse-linked-list.py
+++ camp/exam_week/reverse-linked-list.py
@@ -1,19 +1,22 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return 
+        
         cur = head
-        ans = ListNode()
-        
+        prev = None
+
         while cur:
-            new = ListNode(cur.val)
-            new.next = ans.next
-            ans.next = new            
-            cur = cur.next
-        return ans.next
+            
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur =temp
+            # temp.next
         
+        return prev
